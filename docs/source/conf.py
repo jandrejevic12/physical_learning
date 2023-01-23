f8d9b7ba21ef992a09431bb57df19bfac93c1192
Jovana Andrejevic <jandrejevic@g.harvard.edu>
2023-01-23 13:08:48 -0500

    Fixing link.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,10 +50,6 @@ import sys
 sys.path.insert(0, os.path.abspath('./../..'))
 
 # to display docs when using C-based packages
-import physical_learning
-physical_learning.elastic_utils.Elastic.__name__ = 'Elastic'
-physical_learning.allosteric_utils.Allosteric.__name__ = 'Allosteric'
-
 import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'scipy.integrate', 'scipy.spatial', 'scipy.linalg', 'cmocean', 'networkx',
@@ -62,4 +58,8 @@ MOCK_MODULES = ['numpy', 'scipy', 'scipy.integrate', 'scipy.spatial', 'scipy.lin
 		'plot_imports']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
+
+import physical_learning
+physical_learning.elastic_utils.Elastic.__name__ = 'Elastic'
+physical_learning.allosteric_utils.Allosteric.__name__ = 'Allosteric'
 
