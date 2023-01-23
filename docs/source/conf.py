7782517d8c23d0a8ad9e326105a465510c8c9d56
Jovana Andrejevic <jandrejevic@g.harvard.edu>
2023-01-23 13:39:12 -0500

    Fixing link.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,18 +12,18 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'nbsphinx',
+	'sphinx.ext.duration',
+	'sphinx.ext.doctest',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.autosummary',
+	'sphinx.ext.intersphinx',
+	'sphinx.ext.napoleon',
+	'nbsphinx',
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+	'python': ('https://docs.python.org/3/', None),
+	'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
@@ -50,29 +50,30 @@ import sys
 sys.path.insert(0, os.path.abspath('./../..'))
 
 # to display docs when using C-based packages
-
 def setup(app):
-    import mock
-	from physical_learning import plot_imports, packing_utils, elastic_utils, allosteric_utils, lammps_utils
-	# need to assign the names here, otherwise autodoc won't document these classes,
-    # and will instead just say 'alias of ...'
-    packing_utils.Packing.__name__ = 'Packing'
-    elastic_utils.Elastic__name__ = 'Elastic'
-    allosteric_utils.Allosteric.__name__ = 'Allosteric'
-    
+	import mock
+
 	MOCK_MODULES = ['numpy', 'scipy', 'scipy.integrate', 'scipy.spatial', 'scipy.linalg', 'cmocean', 'networkx',
 			'numba', 'pandas', 'poisson_disc', 'skimage', 'sklearn', 'tqdm',
 			'matplotlib', 'matplotlib.pyplot', 'matplotlib.collections', 'matplotlib.animation', 'matplotlib.ticker']
 
+	for mod_name in MOCK_MODULES:
+		sys.modules[mod_name] = mock.Mock()
+
+	from physical_learning import plot_imports, packing_utils, elastic_utils, allosteric_utils, lammps_utils
+	# need to assign the names here, otherwise autodoc won't document these classes,
+	# and will instead just say 'alias of ...'
+	packing_utils.Packing.__name__ = 'Packing'
+	elastic_utils.Elastic__name__ = 'Elastic'
+	allosteric_utils.Allosteric.__name__ = 'Allosteric'
+
 	REAL_MODULES = ['plot_imports', 'packing_utils', 'elastic_utils', 'allosteric_utils', 'lammps_utils']
 
-	for mod_name in MOCK_MODULES:
-	    sys.modules[mod_name] = mock.Mock()
 	for mod_name in REAL_MODULES:
 		sys.modules[mod_name] = eval(mod_name)
 
-    app.connect('build-finished', build_finished)
+	app.connect('build-finished', build_finished)
 
 def build_finished(app, exception):
-    pass
+	pass
 
